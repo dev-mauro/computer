1db1cec8500f748b5ccaad8472b5b9e408366d19
Mauro Silva <silvac.mauro@gmail.com>
2023-01-14 12:07:26 -0300

    Correcciones entrega final

--- src/components/ItemDetailImage.jsx
+++ src/components/ItemDetailImage.jsx
@@ -1,8 +1,8 @@
 const ItemDetailImage = ({ imageURL }) => {
   return (
-    <figure className="w-full md:w-1/2 h-96 ring-1 ring-slate-200 mb-10 md:mb-0 dark:ring-teal-400">
+    <figure className="w-full md:w-1/2 h-96 ring-1 ring-slate-200 mb-10 md:mb-0 dark:ring-teal-400 overflow-hidden flex justify-center">
 
-      <img src={imageURL} alt="" />
+      <img src={imageURL} alt="" className="h-96 max-w-fit"/>
 
     </figure>
   )
